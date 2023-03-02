b9a7548c24000c4016a039ff9eda61d71d48f234
Firemarcel112 <marcel.bartl2@outlook.de>
2023-03-02 21:34:27 +0100

    beginn add my about section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.app', ["title" => "test"])
 
 @section('content')
-    <h1>Hallo</h1>
+    <div class="aboutMe ml-16">
+        <h1 class="text-4xl mb-4">{{ strtoupper(__('home.about')) }}</h1>
+        <p class="text-2xl">{{ __('home.greeting') }}</p>
+    </div>
+
 @endsection
