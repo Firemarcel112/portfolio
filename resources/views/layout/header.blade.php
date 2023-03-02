116167c51df333894b47bd3497dc110c667c629f
Firemarcel112 <marcel.bartl2@outlook.de>
2023-03-02 21:34:00 +0100

    change h1 to h2, move social links to an extra file.

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,13 +6,13 @@
         </figure>
 
         <div class="content">
-            <h1 class="text-6xl mb-6 font-bold">{{ __('header.greeting') }}</h1>
+            <h2 class="text-6xl mb-6 font-bold">{{ __('header.greeting') }}</h2>
             <div class="subcontent flex items-center">
                 <p class="text-xl">{{ strtoupper(__('header.connect')) }}</p>
                 <i class="fa-solid fa-chevron-right text-2xl mx-2 text-yellow-500"></i>
-                <a target="_blank" href="https://github.com/Firemarcel112"><i class="fa-brands fa-github"></i></a>
-                <a target="_blank" href="https://www.linkedin.com/in/marcel-bartl-20aa01265/"><i class="fa-brands fa-linkedin" style="color:#0077B5"></i></a>
-                <a target="_blank" href="https://www.discordapp.com/users/307931032559157248" style="color: #738ADB"><i class="fa-brands fa-discord"></i></a>
+                <a target="_blank" href="{{ __('links.github') }}"><i class="fa-brands fa-github"></i></a>
+                <a target="_blank" href="{{ __('links.linkedin') }}"><i class="fa-brands fa-linkedin" style="color:#0077B5"></i></a>
+                <a target="_blank" href="{{ __('links.discord') }}" style="color: #738ADB"><i class="fa-brands fa-discord"></i></a>
             </div>
 
         </div>
