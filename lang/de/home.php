52475e9887371f67eafcb5c027ba9d30c6047df4
Firemarcel112 <marcel.bartl2@outlook.de>
2023-03-25 15:14:47 +0100

    add aboutMe text

--- lang/de/home.php
+++ lang/de/home.php
@@ -2,5 +2,6 @@
 
     return [
         'about' => 'Wer bin ich?',
-        'greeting' => 'Ich bin Marcel Bartl, Informationstechnischer Assistent & Web Entwickler'
+        'greeting' => 'Ich bin Marcel Bartl, Angehender Informationstechnischer Assistent',
+        'informationAboutMe' => 'Hallo! Mein Name ist Marcel und ich mache aktuell eine Ausbildung zum Informationstechnischen Assistenten an der Sabine Blindow Schule in Hannover. Ich arbeite im Bereich Webentwicklung und habe Erfahrung mit PHP. In meiner Arbeit habe ich erste Erfahrungen im E-Commerce mit Shopware gemacht. Zudem arbeite ich mit dem Framework Laravel und TailwindCSS. Ich programmiere seit über 2,5 Jahren und bin ständig auf der Suche nach neuen Herausforderungen und Möglichkeiten, meine Fähigkeiten zu verbessern. In meiner Freizeit engagiere ich mich bei der Feuerwehr.'
     ];
--- lang/en/home.php
+++ lang/en/home.php
@@ -2,5 +2,6 @@
 
     return [
         'about' => 'Who am I?',
-        'greeting' => 'I\'m Marcel Bartl, a Information technology assistant & Web developer'
+        'greeting' => 'I\'m Marcel Bartl, a trainee IT-Assistant.',
+        'informationAboutMe' => 'Hello! My name is Marcel and I am currently studying to become an Information Technology Assistant at the Sabine Blindow School in Hanover. I work in web development and have experience with PHP. In my work, I have gained initial experience in e-commerce with Shopware. I also work with the Laravel framework and TailwindCSS. I have been programming for over 2.5 years and am constantly looking for new challenges and opportunities to improve my skills. In my free time, I volunteer at the fire department.',
     ];
